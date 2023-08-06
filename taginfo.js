2e81b934ff17c7cf31a663d6902ff93f167ea19b
endolesuda <endolesuda@MacBook-Pro-2.local>
2023-08-07 00:09:14 +0800

    first commit

---
+++ taginfo.js
@@ -0,0 +1,80 @@
+{
+  "data_format": 1,
+  "data_url": "http://openbeermap.github.io/taginfo.json",
+  "project": {
+    "name": "OpenBeerMap",
+    "description": "The free and editable map of draft beers",
+    "project_url": "http://openbeermap.github.io/",
+    "icon_url": "http://openbeermap.github.io/assets/img/application/ffos-16.png",
+    "contact_name": "Noémie Lehuby",
+    "contact_email": "openbeermap@gmail.com"
+  },
+  "tags": [
+    {
+      "key": "brewery",
+      "object_types": ["area", "node"],
+      "description": "draft beers served : displayed on the map ; can be edited"
+    },
+    {
+      "key": "brewery",
+      "value": "none",
+      "object_types": ["area", "node"],
+      "description": "excluded from display on the map"
+    },
+    {
+      "key": "brewery:note",
+      "object_types": ["area", "node"],
+      "description": "note on draft beers : created by the project ; included to brewery after moderation ; can be edited"
+    },
+    {
+      "key": "name",
+      "object_types": ["area", "node"],
+      "description": "name of the bar : displayed on the map ; can be edited"
+    },
+    {
+      "key": "internet_access",
+      "object_types": ["area", "node"],
+      "description": "displayed on the map ; can be edited"
+    },
+    {
+      "key": "opening_hours",
+      "object_types": ["area", "node"],
+      "description": "displayed on the map ; cannot be edited"
+    },
+    {
+      "key": "happy_hours",
+      "object_types": ["area", "node"],
+      "description": "displayed on the map ; cannot be edited"
+    },    
+    {
+      "key": "amenity",
+      "value": "bar",      
+      "object_types": ["area", "node"],
+      "description": "displayed on the map ; cannot be edited"
+    }, 
+    {
+      "key": "amenity",
+      "value": "cafe",      
+      "object_types": ["area", "node"],
+      "description": "displayed on the map if not cuisine = coffee_shop; cannot be edited"
+    }, 
+    {
+      "key": "amenity",
+      "value": "biergarten",      
+      "object_types": ["node"],
+      "description": "displayed on the map ; cannot be edited"
+    },     
+    {
+      "key": "microbrewery",
+      "value": "yes",      
+      "object_types": ["node"],
+      "description": "displayed on the map if tagged on an amenity; cannot be edited"
+    },     
+    {
+      "key": "amenity",
+      "value": "pub",      
+      "object_types": ["area", "node"],
+      "description": "displayed on the map ; cannot be edited"
+    }
+  ]
+}
